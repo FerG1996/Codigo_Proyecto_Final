e73dd288f3e2393e92d31e1dcf2e65a7d02eac7f
Fernando <fernandoguerrero662@gmail.com>
2021-05-25 01:36:38 +0100

    Ultima version y archivo de salida

--- Prueba-Fer.c
+++ Prueba-Fer.c
@@ -14,7 +14,9 @@
 // with this file, You can obtain one at https://www.gnu.org/licenses/lgpl-3.0.en.html
 
 #include "AD-DA-WS-RPI/AD-DA-WS-RPI.h"
-
+#include <sys/time.h>  				// elapsed time to microseconds; measure sample rate
+#include <time.h> 
+#include <unistd.h>
 /**
  * @brief Simple code to get the voltage on each 8 inputs (single ended) using the ADC1256 and write the input 1 value to output 1 using DAC8552.
  * You can use the jumper of the waveshare board to connect to 5V (power supply and vref), and connect ADO to ADJ (the potentiometer) and DAC to LEDA.
@@ -23,9 +25,9 @@
  */
 int main(void)
 {
-	printf("Empezando prueba por el Fer\r\n");
+	printf("Iniciando prueba\r\n");
 
-	int initSpi = spi_init();
+	int initSpi = spi_init();													//Inicializamos la comunicación SPI
         if (initSpi != 1)
 	{
 		printf("SPI init failed with code %d\r\n", initSpi);
@@ -34,57 +36,57 @@ int main(void)
 	printf("SPI initialized\r\n");
 
 	//Prepare an array with the 8 single ended inputs to read [0,1,2,3,4,5,6,7]
-	int NbChannels = 8;
-	int MainLoop = 0;
+	int NbChannels = 8;															//Cantidad de canales a leer
+	int MainLoop = 0;															//Variables de control (sacar despues)			
 	int RetCode = 0;
-	uint8_t *Channels = malloc(NbChannels * sizeof(uint8_t));
+	FILE *fp;																	//Descriptor de archivo que usaremos para guardar los datos en un archivo
+	int CantMuestras = 5;														//Cantidad de muestras a leer por cada canal (Deberá ser un parámetro que ingrese el usuario)
+	uint8_t *Channels = malloc(NbChannels * sizeof(uint8_t));					//Arreglo dinámico con la cantidad de canales a leer
 	uint8_t Ch;
-	for (Ch = 0; Ch < NbChannels; Ch++)
-	{
+	for (Ch = 0; Ch < NbChannels; Ch++)											//Rellenamos el arreglo con los nros que representan a cada canal, de 0 a 7 (NbChannels-1). ¿Se podría hacer con 
+	{																			//una simple constante que se incremente cada vez que se quiera cambiar de canal?
 		*(Channels + Ch) = Ch;
 	}
-
-	//Write 0 to ouputs A and B
-	DAC8552_Write(channel_A, Voltage_Convert(5.0, 0));
-	DAC8552_Write(channel_B, Voltage_Convert(5.0, 0));
 
 	while (1 == 1)
 	{
 
 		printf("ADC_DAC_Init\r\n");
 		int Id = 0;
-		int Init = ADC_DAC_Init(&Id, ADS1256_GAIN_1, ADS1256_100SPS);
-		if (Init != 0)
+		int Init = ADC_DAC_Init(&Id, ADS1256_GAIN_1, ADS1256_100SPS);			//Inicializamos el ADC. En Id obtenemos el numero de identificación del chip que debería ser igual a 3.
+		if (Init != 0)															//Elegimos ganancia igual a 1 y frecuencia de muestreo igual a 100 m/s
 		{
 			RetCode = -1;
 			break;
 		}
 		printf("init done !\r\n");
+		fp=fopen("Lectura.txt","a");
+		//fp = fopen("/home/Desktop/Archivos/libreria/RaspberryPi-ADC-DAC-master/Repo/Codigo_Proyecto_Final", "Lecturas");		//Abrimos un archivo de nombre "Lectura" en la ruta especificada
 		int Loop;
-		for (Loop = 0; Loop < 100; Loop++)
+		for (Loop = 0; Loop < CantMuestras; Loop++)										
 		{
-			int32_t *AdcValues = NULL;
-			ADS1256_ReadAdcValues(&Channels, NbChannels, SINGLE_ENDED_INPUTS_8, &AdcValues);
+			int32_t *AdcValues = NULL;																		//Arreglo dinámico donde guardaremos los valores leidos de cada canal(se rellena con los argumentos pasados en ReadAdcValues
+			ADS1256_ReadAdcValues(&Channels, NbChannels, SINGLE_ENDED_INPUTS_8, &AdcValues);				//Pasamos el arreglo con los canales a leer, la cant. de canales, el modo (singular o diferencial) y el arreglo 
+																											//donde guardar los valores leidos
+			double *volt = ADS1256_AdcArrayToMicroVolts(AdcValues, NbChannels, 1.0 / 1000000.0);			//Pasamos a volts los valores leidos (esto lo podemos realizar en un excel una vez que saquemos los datos para poder
+																											//reducir los ciclos de procesamiento del procesador
+			
+			fprintf(fp,"0 : %f V   1 : %f V   2 : %f V   3 : %f V   4 : %f V   5 : %f V   6 : %f V   7 : %f V \r\n",
+				   volt[0], volt[1], volt[2], volt[3], volt[4], volt[5], volt[6], volt[7]);  //Imprimimos los valores del arreglo en el archivo (Probar)
+			//printf("0 : %f V   1 : %f V   2 : %f V   3 : %f V   4 : %f V   5 : %f V   6 : %f V   7 : %f V \r\n",		//Mostrar los datos por puerto serie
+			//	   volt[0], volt[1], volt[2], volt[3], volt[4], volt[5], volt[6], volt[7]);
 
-			double *volt = ADS1256_AdcArrayToMicroVolts(AdcValues, NbChannels, 1.0 / 1000000.0);
+			//DAC8552_Write(channel_A, Voltage_Convert(5.0, volt[0]));										//Funcion para escribir en una salida el valor leido por el canal 0
 
-			printf("0 : %f V   1 : %f V   2 : %f V   3 : %f V   4 : %f V   5 : %f V   6 : %f V   7 : %f V \r\n",
-				   volt[0], volt[1], volt[2], volt[3], volt[4], volt[5], volt[6], volt[7]);
-
-			DAC8552_Write(channel_A, Voltage_Convert(5.0, volt[0]));
-
-			free(AdcValues);
+			free(AdcValues);																				//Así como inicializamos los arreglos dinamicos y reservamos memoria, tambien debemos liberar esa memoria reservada
 			free(volt);
-
-			bsp_DelayUS(500000);
+			printf("aca llega!\r\n");
+			bsp_DelayUS(500000);																			//Espera activa de 0,5 segundos = 500000 uS
 		}
-
-		//Write 0 to ouputs A and B
-		DAC8552_Write(channel_A, Voltage_Convert(5.0, 0));
-		DAC8552_Write(channel_B, Voltage_Convert(5.0, 0));
+		fclose(fp);																							//Cerramos el descriptor de archivo
 
 		printf("ADC_DAC_Close\r\n");
-		int CloseCode = ADC_DAC_Close();
+		int CloseCode = ADC_DAC_Close();																	//Finalizamos la comunicación SPI y ponemos al conversor en standby
 		if (CloseCode != 0)
 		{
 			RetCode = -2;
@@ -96,6 +98,7 @@ int main(void)
 		//This loop proves that you can close and re-init pacefully the librairie. Prove it several times (e.g. 3) and then finish the code.
 		if (MainLoop >= 3)
 			break;
+			//break;
 	}
 
 	printf("Test ADDA finished with returned code %d\r\n", RetCode);
