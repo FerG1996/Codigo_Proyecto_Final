6a2aa728cbcf62530d8fde7a66baf0f65086b6a5
Fernando <fernandoguerrero662@gmail.com>
2021-07-16 11:51:19 -0300

    Ultima version

--- Prueba-Fer.c
+++ Prueba-Fer.c
@@ -24,8 +24,7 @@
  * 
  * @return int 
  */
-struct timeval start,canal1;	//Estructuras donde guardaremos el tiempo
-int64_t tiempo1[40000][2];
+struct timeval start,current;	//Estructuras donde guardaremos el tiempo
 
 int lectura(int NbChannels, int AdcValues[][8], ADS1256_SCAN_MODE mode, int loop)
 {
@@ -50,14 +49,7 @@ int lectura(int NbChannels, int AdcValues[][8], ADS1256_SCAN_MODE mode, int loop
 		ADS1256_WriteCmd(CMD_WAKEUP);
 		bsp_DelayUS(MASTER_CLOCK_PERIOD_USEC_TIMES_24);
 		
-		if(i==1)
-		{
-			gettimeofday(&canal1, NULL);      																		// Guardamos el tiempo en la estructura start
-			tiempo1[loop][0]=start.tv_sec;
-			tiempo1[loop][1]=start.tv_usec;
-		}
 		AdcValues[loop][i] = ADS1256_ReadData();
-		
 	}
 	
 	return 0;
@@ -80,17 +72,17 @@ int main(void)
 	int MainLoop = 0;															//Variables de control (sacar despues)			
 	int RetCode = 0;
 	FILE *fp;																	//Descriptor de archivo que usaremos para guardar los datos en un archivo
-	//FILE *fp1;
+//	FILE *fp1;
 	
 	int64_t tiempo[40000][2];													//Matriz donde guardaremos el tiempo en segundos y microsegundos
-	int CantMuestras = 39000;														//Cantidad de muestras a leer por cada canal (Deberá ser un parámetro que ingrese el usuario)
+	int CantMuestras = 1000;														//Cantidad de muestras a leer por cada canal (Deberá ser un parámetro que ingrese el usuario)
 
 	while (1 == 1)
 	{
 
 		printf("ADC_DAC_Init\r\n");
 		int Id = 0;
-		int Init = ADC_DAC_Init(&Id, ADS1256_GAIN_1, ADS1256_30000SPS);			//Inicializamos el ADC. En Id obtenemos el numero de identificación del chip que debería ser igual a 3.
+		int Init = ADC_DAC_Init(&Id, ADS1256_GAIN_1, ADS1256_15000SPS);			//Inicializamos el ADC. En Id obtenemos el numero de identificación del chip que debería ser igual a 3.
 		if (Init != 0)															//Elegimos ganancia igual a 1 y frecuencia de muestreo igual a 100 m/s
 		{
 			RetCode = -1;
@@ -99,19 +91,26 @@ int main(void)
 		printf("init done !\r\n");
 		printf("clocks por seg = %ld\r\n",CLOCKS_PER_SEC);
 		fp=fopen("Lectura.txt","a+");
-		fprintf(fp,"Canal 0 (uV)	Canal 1 (uV)	Canal 2 (uV)	Canal 3 (uV)	Canal 4 (uV)	Canal 5 (uV)	Canal 6 (uV)	Canal 7 (uV)	Tiempo: canal 0 (S) +	uS	Canal 1 (S) +	uS\r\n"); 		//Encabezado de columnas en el excel
+		gettimeofday(&start, NULL);  
+		fprintf(fp,"%s\n",asctime(localtime(&start.tv_sec)));					//Imprimimos la fecha y la hora actual
+		/*start.tv_sec = 0;														//Ponemos los segundos de la estructura en 0
+		start.tv_usec = 0;														//Ponemos los microsegundos en 0
+		settimeofday(&start, NULL);		*/										//Seteamos la estructura con los valores colocados
+		fprintf(fp,"Volts/cuenta =	5000000/8388608\nCanal 0 (uV)	Canal 1 (uV)	Canal 2 (uV)	Canal 3 (uV)	Canal 4 (uV)	Canal 5 (uV)	Canal 6 (uV)	Canal 7 (uV)	Tiempo: canal 0 (S) +	uS\r\n"); 		//Encabezado de columnas en el excel
 		//fp=fopen("Lectura.txt","a");																							//Adjuntamos un archivo de nombre "Lectura", para escritura. Se crea si no existe
 		//fp = fopen("/home/Desktop/Archivos/libreria/RaspberryPi-ADC-DAC-master/Repo/Codigo_Proyecto_Final", "Lecturas");		//Abrimos un archivo de nombre "Lectura" en la ruta especificada
 		int Loop;
 		//int cont;
 		int32_t AdcValues[40000][8];															//Arreglo donde guardaremos los valores leidos de cada canal(se rellena con los argumentos pasados en ReadAdcValues
 		//int32_t *AdcValues = NULL;
+		gettimeofday(&start, NULL);  
+		fprintf(fp,"\nTiempo inicial = %ld s	%ld us\n",start.tv_sec,start.tv_usec);
 		for (Loop = 0; Loop < CantMuestras; Loop++)										
 		{
 			
-			gettimeofday(&start, NULL);      																		// Guardamos el tiempo en la estructura start
-			tiempo[Loop][0]=start.tv_sec;
-			tiempo[Loop][1]=start.tv_usec;
+			gettimeofday(&current, NULL);      																		// Guardamos el tiempo en la estructura start
+			tiempo[Loop][0]=current.tv_sec;
+			tiempo[Loop][1]=current.tv_usec;
 			
 			lectura(NChannels,AdcValues,SINGLE_ENDED_INPUTS_8,Loop);
 			//ADS1256_ReadAdcValues(&Channels, NChannels, SINGLE_ENDED_INPUTS_8, &AdcValues);			//Pasamos el arreglo con los canales a leer, la cant. de canales, el modo (singular o diferencial) y el arreglo 
@@ -122,8 +121,8 @@ int main(void)
 		for (Loop = 0; Loop < CantMuestras; Loop++)										
 		{
 				//double *volt = ADS1256_AdcArrayToMicroVolts(AdcValues, NChannels, 1000000.0 / 1000000.0);					//Pasamos a volts los valores leidos (esto lo podemos realizar en un excel una vez que saquemos los datos para poder																										//reducir los ciclos de procesamiento del procesador
-				fprintf(fp,"%d		%d		%d		%d		%d		%d		%d		%d			%lld   %lld	  %lld	 %lld  \r\n",AdcValues[Loop][0], AdcValues[Loop][1], AdcValues[Loop][2], AdcValues[Loop][3], AdcValues[Loop][4], AdcValues[Loop][5], 
-					AdcValues[Loop][6], AdcValues[Loop][7], tiempo[Loop][0], tiempo[Loop][1], tiempo1[Loop][0],tiempo1[Loop][1] ); 		// tiempo/(double)CLOCKS_PER_SEC
+				fprintf(fp,"%d	%d	%d	%d	%d	%d	%d	%d	%lld	%lld\r\n",AdcValues[Loop][0], AdcValues[Loop][1], AdcValues[Loop][2], AdcValues[Loop][3], AdcValues[Loop][4], AdcValues[Loop][5], 
+					AdcValues[Loop][6], AdcValues[Loop][7], tiempo[Loop][0], tiempo[Loop][1]); 		// tiempo/(double)CLOCKS_PER_SEC
 				
 		}
 		fclose(fp);																							//Cerramos el descriptor de archivo
